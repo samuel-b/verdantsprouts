39cd0bc4772f8b3046e7466b9e4427bcf5673f8c
samuel-b <signups@samuel-b.com>
2023-12-31 15:27:25 +1100

    basic nav and footer

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,8 +3,16 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
 })
 export class NavbarComponent {
-
+  /* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
+  myFunction() {
+    var x = document.getElementById('navbar');
+    if (x?.className === 'navbar') {
+      x.className += ' responsive';
+    } else {
+      x!.className = 'navbar';
+    }
+  }
 }
